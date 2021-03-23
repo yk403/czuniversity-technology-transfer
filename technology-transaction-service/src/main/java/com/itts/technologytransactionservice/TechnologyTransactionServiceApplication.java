83773e4e0ecc3618a7062c41ed46723627fdc070
刘英名 <601171179@qq.com>
2021-03-23 15:38:59 +0800

    修改：测试环境：包扫描配置

--- technology-transaction-service/src/main/java/com/itts/technologytransactionservice/TechnologyTransactionServiceApplication.java
+++ technology-transaction-service/src/main/java/com/itts/technologytransactionservice/TechnologyTransactionServiceApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"com.itts"})
+@SpringBootApplication(scanBasePackages = {"com.itts.technologytransactionservice",
+        "com.itts.common.exception", "com.itts.common.utils", "com.itts.common.config"})
 @MapperScan("com.itts.technologytransactionservice.*")
 @EnableDiscoveryClient
 @EnableFeignClients
