595c7e91f8f72c94b941444a86c65db974af5a97
北往南 <8594216+fulifuli@user.noreply.gitee.com>
2021-04-01 09:01:28 +0800

    修改：用户服务：数据字典管理

--- user-service/src/main/java/com/itts/userservice/controller/shzd/ShzdController.java
+++ user-service/src/main/java/com/itts/userservice/controller/shzd/ShzdController.java
@@ -5,11 +5,10 @@ import com.itts.common.constant.SystemConstant;
 import com.itts.common.enums.ErrorCodeEnum;
 import com.itts.common.exception.WebException;
 import com.itts.common.utils.common.ResponseUtil;
-import com.itts.userservice.model.jggl.Jggl;
 import com.itts.userservice.model.shzd.Shzd;
 import com.itts.userservice.service.shzd.ShzdService;
 import io.swagger.annotations.ApiOperation;
-import net.sf.jsqlparser.expression.LongValue;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,55 +18,60 @@ import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author fl
  * @since 2021-03-31
  */
+@Slf4j
 @RestController
-@RequestMapping(SystemConstant.ADMIN_BASE_URL+"/shzd")
+@RequestMapping(SystemConstant.ADMIN_BASE_URL + "/shzd")
 public class ShzdController {
 
     @Resource
     private ShzdService shzdService;
+
     /**
      * 通过名称或编码查询
      */
     @GetMapping("/queryDictionary/{string}")
     @ApiOperation(value = "通过名称或编码查询")
-    public ResponseUtil getByNameAndCode(@PathVariable("string")String string)throws WebException{
+    public ResponseUtil getByNameAndCode(@PathVariable("string") String string) throws WebException {
         Shzd shzd = shzdService.selectByString(string);
         return ResponseUtil.success(shzd);
     }
+
     /**
      * 新增
      */
     @PostMapping("/add/")
     @ApiOperation(value = "新增")
-    public ResponseUtil add(@RequestBody Shzd shzd)throws WebException{
+    public ResponseUtil add(@RequestBody Shzd shzd) throws WebException {
         checkPequest(shzd);
         Shzd add = shzdService.add(shzd);
         return ResponseUtil.success(add);
     }
+
     /**
      * 更新
      */
-    @ApiOperation(value="更新")
+    @ApiOperation(value = "更新")
     @PutMapping("/update/{id}")
-    public ResponseUtil update(@PathVariable("id")Long id,@RequestBody Shzd shzd)throws WebException{
-        if(id==null){
+    public ResponseUtil update(@PathVariable("id") Long id, @RequestBody Shzd shzd) throws WebException {
+        if (id == null) {
             throw new WebException(ErrorCodeEnum.SYSTEM_REQUEST_PARAMS_ILLEGAL_ERROR);
         }
         Shzd shzd1 = shzdService.get(id);
-        if(shzd1==null){
+        if (shzd1 == null) {
             throw new WebException(ErrorCodeEnum.SYSTEM_NOT_FIND_ERROR);
         }
         checkPequest(shzd);
-        BeanUtils.copyProperties(shzd,shzd1,"id", "cjsj", "cjr");
+        BeanUtils.copyProperties(shzd, shzd1, "id", "cjsj", "cjr");
         shzdService.update(shzd1);
         return ResponseUtil.success(shzd1);
     }
+
     /**
      * 删除
      */
@@ -79,7 +83,7 @@ public class ShzdController {
             throw new WebException(ErrorCodeEnum.SYSTEM_REQUEST_PARAMS_ILLEGAL_ERROR);
         }
         Shzd shzd = shzdService.get(id);
-        if(shzd==null){
+        if (shzd == null) {
             throw new WebException(ErrorCodeEnum.SYSTEM_NOT_FIND_ERROR);
         }
         shzd.setSfsc(false);
@@ -87,19 +91,21 @@ public class ShzdController {
         shzdService.update(shzd);
         return ResponseUtil.success();
     }
+
     /**
      * 批量删除
      */
-    @ApiOperation(value ="批量删除")
+    @ApiOperation(value = "批量删除")
     @DeleteMapping("/deleteBatch")
-    public ResponseUtil deleteBatch(@RequestBody List<Long> ids){
+    public ResponseUtil deleteBatch(@RequestBody List<Long> ids) {
         if (ids == null) {
             throw new WebException(ErrorCodeEnum.SYSTEM_REQUEST_PARAMS_ILLEGAL_ERROR);
         }
+
         for (int i = 0; i < ids.size(); i++) {
             Shzd shzd = shzdService.get(ids.get(i));
-            if(shzd==null){
-                throw new WebException(ErrorCodeEnum.SYSTEM_NOT_FIND_ERROR);
+            if (shzd == null) {
+                log.error("【数据字典-批量删除】数据字典不存在");
             }
             shzd.setSfsc(false);
             shzd.setGxsj(new Date());
@@ -107,6 +113,7 @@ public class ShzdController {
         }
         return ResponseUtil.success();
     }
+
     /**
      * 校验参数是否合法
      */
