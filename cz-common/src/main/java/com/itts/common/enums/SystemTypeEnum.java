305bdf7552e86945bec39887726834fa1319639a
刘英名 <601171179@qq.com>
2021-04-09 10:34:05 +0800

    修改：用户服务：系统类型枚举

--- cz-common/src/main/java/com/itts/common/enums/SystemTypeEnum.java
+++ cz-common/src/main/java/com/itts/common/enums/SystemTypeEnum.java
@@ -12,22 +12,22 @@ public enum SystemTypeEnum {
     /**
      * 技术交易门户
      */
-    TECHNOLOGY_TRANSACTION_PORTAL("TECHNOLOGY_TRANSACTION_PORTAL", "技术交易门户"),
+    TECHNOLOGY_TRANSACTION_PORTAL("technology_transaction_portal", "技术交易门户"),
 
     /**
      * 技术交易后台管理
      */
-    TECHNOLOGY_TRANSACTION_BACKSTAGE_MANAGEMENT("TECHNOLOGY_TRANSACTION_BACKSTAGE_MANAGEMENT", "技术交易后台管理"),
+    TECHNOLOGY_TRANSACTION_BACKSTAGE_MANAGEMENT("technology_transaction_backstage_management", "技术交易后台管理"),
 
     /**
      * 人才培养门户
      */
-    TALENT_TRAINING_PORTAL("TALENT_TRAINING_PORTAL", "人才培养门户"),
+    TALENT_TRAINING_PORTAL("talent_training_portal", "人才培养门户"),
 
     /**
      * 人才培养后台管理
      */
-    TALENT_TRAINING_BACKSTAGE_MANAGEMENT("TALENT_TRAINING_BACKSTAGE_MANAGEMENT", "人才培养后台管理");
+    TALENT_TRAINING_BACKSTAGE_MANAGEMENT("talent_training_backstage_management", "人才培养后台管理");
 
     private String key;
 
