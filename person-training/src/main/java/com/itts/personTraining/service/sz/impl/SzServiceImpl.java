876241002c158787343fd17593b419f47a19e2a8
“xiaogang” <gang3530926@163.com>
2021-04-20 11:30:00 +0800

    新增：人才培养服务: 师资实体类及结构

---
+++ person-training/src/main/java/com/itts/personTraining/service/sz/impl/SzServiceImpl.java
@@ -0,0 +1,20 @@
+package com.itts.personTraining.service.sz.impl;
+
+import com.itts.personTraining.model.Sz;
+import com.itts.personTraining.mapper.sz.SzMapper;
+import com.itts.personTraining.service.sz.SzService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 师资表 服务实现类
+ * </p>
+ *
+ * @author Austin
+ * @since 2021-04-20
+ */
+@Service
+public class SzServiceImpl extends ServiceImpl<SzMapper, Sz> implements SzService {
+
+}
