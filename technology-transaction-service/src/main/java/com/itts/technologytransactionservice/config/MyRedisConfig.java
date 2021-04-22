46502246dea20cbc51fa5e61befca3c0d38e4487
刘英名 <601171179@qq.com>
2021-04-22 08:53:41 +0800

    修改：技术交易：叫价模块修改,使用rabbitMQ

--- technology-transaction-service/src/main/java/com/itts/technologytransactionservice/config/MyRedisConfig.java
+++ technology-transaction-service/src/main/java/com/itts/technologytransactionservice/config/MyRedisConfig.java
@@ -19,20 +19,20 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 @Configuration
 public class MyRedisConfig {
 
-    @Bean
+    /*@Bean
     public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
                                                    MessageListenerAdapter listenerAdapter) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.addMessageListener(listenerAdapter, new PatternTopic(MQConstant.TECHNOLOGY_TRANSACTION_BID_CHANNEL));
         return container;
-    }
+    }*/
 
     /**
      * 绑定消息监听者和接收监听的方法,必须要注入这个监听器，不然会报错
      */
-    @Bean
+   /* @Bean
     public MessageListenerAdapter listenerAdapter(){
         return new MessageListenerAdapter(new BidController(),"receiveMessage");
-    }
+    }*/
 }
