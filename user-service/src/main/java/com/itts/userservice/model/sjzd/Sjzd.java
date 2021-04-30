3f38f62815542dd2a93a1f0904a7c13bad46c629
刘英名 <601171179@qq.com>
2021-04-30 14:40:29 +0800

    修改：用户服务：数据字典时间展示格式化

--- user-service/src/main/java/com/itts/userservice/model/sjzd/Sjzd.java
+++ user-service/src/main/java/com/itts/userservice/model/sjzd/Sjzd.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,7 +23,6 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = false)
 @TableName("t_sjzd")
 public class Sjzd implements Serializable {
-
 
     private static final long serialVersionUID = -635139985468084264L;
     /**
@@ -74,6 +74,7 @@ public class Sjzd implements Serializable {
     /**
      * 创建时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date cjsj;
 
     /**
@@ -84,6 +85,7 @@ public class Sjzd implements Serializable {
     /**
      * 更新时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date gxsj;
 
     /**
