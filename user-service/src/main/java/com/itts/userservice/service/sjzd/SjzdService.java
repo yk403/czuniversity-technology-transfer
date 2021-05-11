f52e88c0384b8284c9d249cc77eea84eb67ce730
刘英名 <601171179@qq.com>
2021-05-11 11:04:28 +0800

    修改：用户服务：数据字典功能优化（增加父级关联）

--- user-service/src/main/java/com/itts/userservice/service/sjzd/SjzdService.java
+++ user-service/src/main/java/com/itts/userservice/service/sjzd/SjzdService.java
@@ -2,7 +2,12 @@ package com.itts.userservice.service.sjzd;
 
 import com.github.pagehelper.PageInfo;
 import com.itts.userservice.model.sjzd.Sjzd;
+import com.itts.userservice.request.sjzd.AddSjzdRequest;
+import com.itts.userservice.request.sjzd.GetSjzdRequest;
+import com.itts.userservice.request.sjzd.UpdateSjzdRequest;
 import com.itts.userservice.vo.SjzdModelVO;
+
+import java.util.List;
 
 /**
  * <p>
@@ -20,6 +25,11 @@ public interface SjzdService {
     PageInfo<SjzdModelVO> findDictionaryModel(Integer pageNum, Integer pageSize, String model, String systemType, String condition);
 
     /**
+     * 通过所属模块获取列表
+     */
+    List<Sjzd> findBySsmk(String xtlb, String mklx, String ssmk);
+
+    /**
      * 获取列表 - 分页
      */
     PageInfo<Sjzd> findByPage(Integer pageNum, Integer pageSize, String model, String systemType, String dictionary);
@@ -30,6 +40,11 @@ public interface SjzdService {
     Sjzd get(Long id);
 
     /**
+     * 通过所属模块获取数据
+     */
+    GetSjzdRequest get(String xtlb, String mklx, String ssmk);
+
+    /**
      * 查询，通过名称或编码
      */
     PageInfo<Sjzd> selectByString(Integer pageNum, Integer pageSize, String string, String ssmk);
@@ -37,10 +52,15 @@ public interface SjzdService {
     /**
      * 新增
      */
-    Sjzd add(Sjzd sjzd);
+    AddSjzdRequest add(AddSjzdRequest sjzd);
 
     /**
      * 更新
      */
-    Sjzd update(Sjzd sjzd);
+    UpdateSjzdRequest update(UpdateSjzdRequest sjzd);
+
+    /**
+     * 删除
+     */
+    void delete(Long id);
 }
