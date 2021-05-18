e354f8f46179c01d93cd5ec07c166517711f3f49
yu kai <1192893425@qq.com>
2021-05-18 16:59:39 +0800

    新增: 双创路演: 路演展位crud

---
+++ technology-transaction-service/src/main/java/com/itts/technologytransactionservice/mapper/LyZwMapper.java
@@ -0,0 +1,27 @@
+package com.itts.technologytransactionservice.mapper;
+
+import com.itts.technologytransactionservice.model.LyLy;
+import com.itts.technologytransactionservice.model.LyZw;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yukai
+ * @since 2021-05-18
+ */
+public interface LyZwMapper extends BaseMapper<LyZw> {
+    /**
+     * 分页条件查询需求
+     *
+     * @param map
+     * @return
+     */
+    List<LyZw> findLyZwFront(@Param("map") Map map);
+}
---
+++ technology-transaction-service/src/main/java/com/itts/technologytransactionservice/service/LyZwService.java
@@ -0,0 +1,22 @@
+package com.itts.technologytransactionservice.service;
+
+import com.github.pagehelper.PageInfo;
+import com.itts.technologytransactionservice.model.LyZw;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yukai
+ * @since 2021-05-18
+ */
+public interface LyZwService extends IService<LyZw> {
+    PageInfo findLyZwFront(Map<String, Object> params);
+
+    Boolean saveZw(LyZw lyZw);
+    Long getUserId();
+}
