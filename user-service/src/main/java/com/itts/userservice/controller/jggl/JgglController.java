e71a617fc1c8b886af27f89a5fe6b4d60d2ab777
北往南 <8594216+fulifuli@user.noreply.gitee.com>
2021-07-09 08:47:57 +0800

    修改：用户：机构管理

--- user-service/src/main/java/com/itts/userservice/controller/jggl/JgglController.java
+++ user-service/src/main/java/com/itts/userservice/controller/jggl/JgglController.java
@@ -297,7 +297,6 @@ public class JgglController {
                             }
                         }
                     }
-
                 }
             }
             jggl.setGxsj(new Date());
