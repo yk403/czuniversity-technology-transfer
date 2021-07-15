47e65f5d95bb31cd8f5341dd6382412791a4d7fb
Austin <ZXG@921106>
2021-07-15 19:37:04 +0800

    修改: 人才培养: 学生成绩批量下发提示;

--- cz-common/src/main/java/com/itts/common/enums/ErrorCodeEnum.java
+++ cz-common/src/main/java/com/itts/common/enums/ErrorCodeEnum.java
@@ -125,6 +125,7 @@ public enum ErrorCodeEnum {
     STARTWEEK_GREATER_THAN_ENDWEEK(-45050, "起始周不可大于结束周!"),
     SIGN_UP_FAIL(-45051,"培训报名失败!"),
     CONDITION_IS_EMPTY_ERROR(-45052,"查询条件不可为空!"),
+    STUDENT_NOT_SELECTED(-45053,"未选择学生，无法下发!"),
     //====================      技术交易服务错误提示  END     ====================
 
     //====================       用户服务错误提示（服务码440）      ====================
