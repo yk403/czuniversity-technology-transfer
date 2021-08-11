6b4262526dca7b2a3f9e1832e3d6030f6d456ee2
Austin <ZXG@921106>
2021-08-11 11:09:33 +0800

    修改：人才培养：前台技术需求BUG;

--- person-training/src/main/java/com/itts/personTraining/model/zj/Zj.java
+++ person-training/src/main/java/com/itts/personTraining/model/zj/Zj.java
@@ -203,6 +203,7 @@ public class Zj implements Serializable {
      * 创建时间
      */
     @TableField(value = "cjsj", fill = FieldFill.INSERT) // 新增执行
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date cjsj;
 
     /**
@@ -214,6 +215,7 @@ public class Zj implements Serializable {
      * 更新时间
      */
     @TableField(value = "gxsj", fill = FieldFill.INSERT_UPDATE) // 新增和更新执行
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date gxsj;
 
     /**
