b2740c2cd9dcc45fd3c16efc114df8b899105d69
Austin <ZXG@921106>
2021-08-21 15:50:03 +0800

    新增：人才培养: 经纪人培训计划门户接口;

--- person-training/src/main/java/com/itts/personTraining/controller/jjrpxjh/user/JjrpxjhController.java
+++ person-training/src/main/java/com/itts/personTraining/controller/jjrpxjh/user/JjrpxjhController.java
@@ -1,5 +1,8 @@
 package com.itts.personTraining.controller.jjrpxjh.user;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.itts.common.constant.SystemConstant;
 import com.itts.common.enums.ErrorCodeEnum;
 import com.itts.common.exception.WebException;
@@ -11,8 +14,12 @@ import com.itts.personTraining.service.jjrpxjh.JjrpxjhService;
 import com.itts.personTraining.vo.jjrpxjh.GetJjrpxjhVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.itts.common.enums.ErrorCodeEnum.SIGN_UP_FAIL;
 
@@ -58,4 +65,24 @@ public class JjrpxjhController {
         }
         return ResponseUtil.success("培训报名成功!");
     }
+
+    @ApiOperation(value = "获取列表")
+    @GetMapping("/list/")
+    public ResponseUtil list(@ApiParam(value = "当前页码") @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                             @ApiParam(value = "每页显示记录数") @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+                             @ApiParam(value = "名称") @RequestParam(value = "mc", required = false) String mc,
+                             @ApiParam(value = "批次") @RequestParam(value = "pcId", required = false) Long pcId) {
+
+        PageHelper.startPage(pageNum, pageSize);
+
+        List<Jjrpxjh> list = jjrpxjhService.list(new QueryWrapper<Jjrpxjh>()
+                .eq("sfsc", false)
+                .like(StringUtils.isNotBlank(mc), "pxjhmc", mc)
+                .eq(pcId != null, "pc_id", pcId).orderByDesc("cjsj"));
+
+        PageInfo pageInfo = new PageInfo(list);
+
+        return ResponseUtil.success(pageInfo);
+    }
+
 }
