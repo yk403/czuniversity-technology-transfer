80deda57f18a07f4f4531a4a41e466ce90ad519d
北往南 <8594216+fulifuli@user.noreply.gitee.com>
2021-08-23 10:04:00 +0800

    修改：人才培养:学历学位成绩excel导入

--- person-training/src/main/java/com/itts/personTraining/dto/XlXwCjDTO.java
+++ person-training/src/main/java/com/itts/personTraining/dto/XlXwCjDTO.java
@@ -40,64 +40,47 @@ public class XlXwCjDTO implements Serializable {
     @ExcelProperty(value = "姓名", index = 1)
     private String xm;
 
-    /**
-     * 论文成绩
-     */
-    @ExcelProperty(value = "论文成绩", index = 2)
-    private String lwcj;
+
 
     /**
      * 课程代码
      */
-    @ExcelProperty(value = "课程代码", index = 3)
+    @ExcelProperty(value = "课程代码", index = 2)
     private String kcdm;
 
 
     /**
      * 是否必修(0:否;1:是)
      */
-    @ExcelProperty(value = "是否必修", index = 4)
+    @ExcelProperty(value = "是否必修", index = 3)
     private Boolean sfbx;
 
-    /**
-     * 学位课(0:否;1:是)
-     */
-    @ExcelProperty(value = "1", index = 5)
-    private Boolean xwk;
 
-    /**
-     * 原专业学分
-     */
-    @ExcelProperty(value = "原专业学分", index = 6)
-    private Integer yzyxf;
+
 
     /**
      * 当前学分
      */
-    @ExcelProperty(value = "当前学分", index = 7)
+    @ExcelProperty(value = "当前学分", index = 4)
     private Integer dqxf;
 
     /**
      * 成绩
      */
-    @ExcelProperty(value = "成绩", index = 8)
+    @ExcelProperty(value = "成绩", index = 5)
     private String cj;
 
-    /**
-     * 选修学期(1-6)
-     */
-    @ExcelProperty(value = "选修学期", index = 9)
-    private String xxxq;
+
 
     /**
      * 成绩属性:(正常,旷考,免考,作弊 重修,缓考,补考, 取消资格)
      */
-    @ExcelProperty(value = "成绩属性", index = 10)
+    @ExcelProperty(value = "成绩属性", index = 6)
     private String cjsx;
 
     /**
      * 补考成绩
      */
-    @ExcelProperty(value = "补考成绩", index = 11)
+    @ExcelProperty(value = "补考成绩", index = 7)
     private String bkcj;
 }
