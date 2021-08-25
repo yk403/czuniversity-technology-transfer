9cd6c156bae66d82c7d7410e58a019cfeb657da9
Austin <ZXG@921106>
2021-08-25 09:44:51 +0800

    修改：人才培养: 批次添加类型;

--- person-training/src/main/java/com/itts/personTraining/service/pc/PcService.java
+++ person-training/src/main/java/com/itts/personTraining/service/pc/PcService.java
@@ -20,7 +20,7 @@ public interface PcService  {
     /**
      * 获取列表 - 分页
      */
-    PageInfo<Pc> findByPage(Integer pageNum, Integer pageSize, String name);
+    PageInfo<Pc> findByPage(Integer pageNum, Integer pageSize, String name, String lx);
     /**
      * 获取通过id
      */
