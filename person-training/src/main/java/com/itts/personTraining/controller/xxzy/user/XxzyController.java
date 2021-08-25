767b2b53850366872e7bef74ddacd234ac486688
北往南 <8594216+fulifuli@user.noreply.gitee.com>
2021-08-25 10:09:11 +0800

    修改：人才培养:学习资源

--- person-training/src/main/java/com/itts/personTraining/controller/xxzy/user/XxzyController.java
+++ person-training/src/main/java/com/itts/personTraining/controller/xxzy/user/XxzyController.java
@@ -27,9 +27,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -121,18 +119,23 @@ public class XxzyController {
                                 @ApiParam(value = "查询条件") @RequestParam(value = "condition", required = false) String condition,
                                 @ApiParam(value = "机构编码") @RequestParam(value = "jgCode", required = false) String jgCode) {
 
-    ResponseUtil childrenResponse = groupFeignService.findChildrenByCode(jgCode);
-    if (childrenResponse == null || childrenResponse.getErrCode().intValue() != 0) {
-      throw new WebException(ErrorCodeEnum.SYSTEM_NOT_FIND_ERROR);
-    }
 
-    List<JgglVO> childrenGroup = childrenResponse.conversionData(new TypeReference<List<JgglVO>>() {
-    });
-    if (CollectionUtils.isEmpty(childrenGroup)) {
-      throw new WebException(ErrorCodeEnum.SYSTEM_NOT_FIND_ERROR);
-    }
+    List<Long> jgIds = new ArrayList<Long>();
+    if(!Objects.equals(jgCode,"001")){
+      ResponseUtil childrenResponse = groupFeignService.findChildrenByCode(jgCode);
+      if (childrenResponse == null || childrenResponse.getErrCode().intValue() != 0) {
+        throw new WebException(ErrorCodeEnum.SYSTEM_NOT_FIND_ERROR);
+      }
 
-    List<Long> jgIds = childrenGroup.stream().map(JgglVO::getId).collect(Collectors.toList());
+      List<JgglVO> childrenGroup = childrenResponse.conversionData(new TypeReference<List<JgglVO>>() {
+      });
+      if (CollectionUtils.isEmpty(childrenGroup)) {
+        throw new WebException(ErrorCodeEnum.SYSTEM_NOT_FIND_ERROR);
+      }
+
+      jgIds = childrenGroup.stream().map(JgglVO::getId).collect(Collectors.toList());
+
+    }
 
     PageInfo<GetXxzyVO> result = xxzyService.findByJgId(pageNum, pageSize, type, firstCategory, secondCategory, category, direction, courseId, condition, jgIds);
 
