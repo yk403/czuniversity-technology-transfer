33a1853807c7b7d952f6526a72f42448de9a1131
北往南 <8594216+fulifuli@user.noreply.gitee.com>
2021-08-31 09:54:40 +0800

    修改：人才培养:注册

---
+++ person-training/src/main/java/com/itts/personTraining/request/yh/RegisterRequest.java
@@ -0,0 +1,43 @@
+package com.itts.personTraining.request.yh;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Description：
+ * @Author：lym
+ * @Date: 2021/3/26
+ */
+@Data
+@ApiModel("用户注册请求参数")
+public class RegisterRequest implements Serializable {
+
+    private static final long serialVersionUID = -8810055754852541015L;
+
+    /**
+     * 用户账号
+     */
+    @ApiModelProperty("用户账号")
+    private String userName;
+
+    /**
+     * 用户密码
+     */
+    @ApiModelProperty("用户密码")
+    private String password;
+
+    /**
+     * 用户类型：in - 内部用户； out - 外部用户
+     */
+    @ApiModelProperty("用户类型：in - 内部用户； out - 外部用户")
+    private String userType;
+
+    /**
+     * 手机号码
+     */
+    @ApiModelProperty("手机号码")
+    private String mobile;
+}
