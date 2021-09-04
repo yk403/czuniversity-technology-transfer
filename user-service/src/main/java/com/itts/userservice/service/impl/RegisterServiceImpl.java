14d74e1b4e75819796e8f4a3a4405b1c73d4690b
北往南 <8594216+fulifuli@user.noreply.gitee.com>
2021-09-04 15:18:58 +0800

    修改：用户服务:登录

--- user-service/src/main/java/com/itts/userservice/service/impl/RegisterServiceImpl.java
+++ user-service/src/main/java/com/itts/userservice/service/impl/RegisterServiceImpl.java
@@ -1,7 +1,10 @@
 package com.itts.userservice.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.itts.userservice.mapper.jggl.JgglMapper;
 import com.itts.userservice.mapper.yh.YhJsGlMapper;
 import com.itts.userservice.mapper.yh.YhMapper;
+import com.itts.userservice.model.jggl.Jggl;
 import com.itts.userservice.model.js.Js;
 import com.itts.userservice.model.yh.Yh;
 import com.itts.userservice.model.yh.YhJsGl;
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.Date;
 
 /**
@@ -28,6 +32,9 @@ public class RegisterServiceImpl implements RegisterService {
 
     @Autowired
     private YhJsGlMapper yhJsGlMapper;
+
+    @Resource
+    private JgglMapper jgglMapper;
 
     /**
      * 注册用户
@@ -46,6 +53,9 @@ public class RegisterServiceImpl implements RegisterService {
         yh.setYhm(request.getUserName());
         yh.setMm(new BCryptPasswordEncoder().encode(request.getPassword()));
         yh.setLxdh(request.getMobile());
+        yh.setYhlx(request.getUserType());
+        Jggl jggl = jgglMapper.selectOne(new QueryWrapper<Jggl>().eq("jgbm", request.getJgCode()).eq("sfsc", false));
+        yh.setJgId(jggl.getId());
         yh.setYhlb(js.getJslb());
         yh.setCjsj(now);
         yh.setGxsj(now);
