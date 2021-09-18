7f73a6ebc4d1ef27a4b0295dd04fcef4cd9aa85e
北往南 <8594216+fulifuli@user.noreply.gitee.com>
2021-09-18 11:06:08 +0800

    修改：技术交易：经纪人

--- technology-transaction-service/src/main/java/com/itts/technologytransactionservice/controller/cd/admin/SjzdAdminController.java
+++ technology-transaction-service/src/main/java/com/itts/technologytransactionservice/controller/cd/admin/SjzdAdminController.java
@@ -4,6 +4,7 @@ import com.itts.common.utils.common.ResponseUtil;
 import com.itts.technologytransactionservice.feign.userservice.SjzdService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +24,27 @@ import static com.itts.common.constant.SystemConstant.ADMIN_BASE_URL;
 @Api(value = "SjzdController", tags = "数据字典")
 public class SjzdAdminController {
 
+
     @Autowired
-    private SjzdService sjzdFeignService;
+    private SjzdService sjzdService;
+    /**
+     * @Description:
+     * @Param: [pageNum, pageSize, model, systemType, dictionary, request]
+     * @return: com.itts.common.utils.common.ResponseUtil
+     * @Author: yukai
+     * @Date: 2021/6/2
+     */
+    @GetMapping("/list/")
+    @ApiOperation(value = "获取类别领域信息")
+    public ResponseUtil getList(@ApiParam(value = "当前页码") @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                                @ApiParam(value = "每页显示记录数") @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+                                @ApiParam(value = "所属模块") @RequestParam(value = "model", required = false) String model,
+                                @ApiParam(value = "所属系统") @RequestParam(value = "systemType", required = false) String systemType,
+                                @ApiParam(value = "字典项类型") @RequestParam(value = "dictionary", required = false) String dictionary,
+                                @ApiParam(value = "字典编码") @RequestParam(value = "zdbm", required = false) String zdbm,
+                                @ApiParam(value = "父级字典ID") @RequestParam(value = "parentId", required = false) Long parentId) {
+        return sjzdService.getList(pageNum,pageSize,model,systemType,dictionary,zdbm,parentId);
+    }
     /**
      * 获取列表
      */
@@ -33,6 +53,6 @@ public class SjzdAdminController {
     public ResponseUtil findList(@RequestParam(value = "xtlb", required = false) String xtlb,
                          @RequestParam(value = "mklx", required = false) String mklx,
                          @RequestParam(value = "ssmk", required = false) String ssmk){
-        return sjzdFeignService.getNotList(xtlb, mklx, ssmk);
+        return sjzdService.getNotList(xtlb, mklx, ssmk);
     }
 }
